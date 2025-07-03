d48223cc4e22bca758ea4cd97ea1c3751ac0efff
Sufairasufi <ashrafsufaira@gmail.com>
2025-07-03 12:57:11 +0530

    changes

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-import { Outlet ,useLocation} from 'react-router-dom'
-import HomeNavbar from './Home/HomeNavbar';
-import Footer from './Footer/Footer';
+import { Outlet, useLocation } from 'react-router-dom'
+import HomeNavbar from './Home/HomeNavbar'
+import Footer from './Footer/Footer'
 
 const Layout = () => {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
+  const location = useLocation()
+  const isHome = location.hash === '#/' || location.pathname === '/'
+
   return (
     <div>
-    {isHome ? <HomeNavbar/> : <Navbar/>}
-    <Outlet/>
-    <Footer/>
+      {isHome ? <HomeNavbar /> : <Navbar />}
+      <Outlet />
+      <Footer />
     </div>
   )
 }
