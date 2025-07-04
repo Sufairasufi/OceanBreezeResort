4e61cc3821f9a2d9b8b87c871bbf0469ac5ec81f
Sufairasufi <ashrafsufaira@gmail.com>
2025-07-04 11:17:43 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/Resort-Booking/',
-    plugins: [react()],
+  base: '/Resort-Booking/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        '404': 'public/404.html'
+      }
+    }
+  }
 })
