8c68ae9ba5a11a50b64016183f0459bcc959af8a
Sufairasufi <ashrafsufaira@gmail.com>
2025-07-04 11:59:00 +0530

    Initial commit for deploy

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Navbar from "./Component/Navbar/Navbar"
+import Home from './Component/Home/Home'
+import Layout from './Component/Layout'
+import About from './Component/About/About'
+import Footer from './Component/Footer/Footer'
+import Contact from './Component/Contact/Contact'
+import Service from './Component/Services/Service'
+import Booking from './Component/Booking/Booking'
+import Luxury from './Component/BookingCategory/Luxury'
+import Cottege from './Component/BookingCategory/Cottege'
+import Family from './Component/BookingCategory/Family'
+import Gallery from './Component/Gallery/Gallery'
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        
+        <Route element={<Layout/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}>
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='service' element={<Service/>}/>
+        <Route path='booking' element={<Booking/>}/>
+        <Route path='luxury' element={<Luxury/>}/>
+        <Route path='cottege' element={<Cottege/>}/>
+        <Route path='family' element={<Family/>}/>
+        <Route path='gallary' element={<Gallery/>}/>
+        </Route>
+        <Route path='/foot' element={<Footer/>}/>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/Resort-Booking/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        '404': 'public/404.html'
+      }
+    }
+  }
+})
